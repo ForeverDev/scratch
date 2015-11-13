6012a655f90d5c49411d933309a8f71469056f82
David Wilson <davidwilson@Davids-iMac.local>
2015-11-13 00:14:03 -0500

    update

--- blocks/indent_block.lua
+++ blocks/indent_block.lua
@@ -17,6 +17,12 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
   local self = G.CLASSES.BASEBLOCK.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
   self.classname = "IndentBlock"
   self.draw_frame = G.CLASSES.FRAME.new(1, 0, 1, 0, 0, 0, 0, 0)
+  -- self.contained is a table that contains blocks stored
+  -- inside this object.  It is like self.children, but it
+  -- only contains other block types.  This is done so that
+  -- the children of this object can easily be positioned
+  -- based on other children
+  self.contained = {}
   -- self.components is a table that contains the positions
   -- of the four frames that are used to draw the
   -- indent block to the screen.  The sizes and positions
@@ -26,7 +32,6 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
     top = G.CLASSES.FRAME.new(0, 0, 0, 0, 0, 0, 0, 0);
     bottom = G.CLASSES.FRAME.new(0, 0, 0, 0, 0, 0, 0, 0);
     left = G.CLASSES.FRAME.new(0, 0, 0, 0, 0, 0, 0, 0);
-    right = G.CLASSES.FRAME.new(0, 0, 0, 0, 0, 0, 0, 0);
   }
 
   -- override UI.SetColor(...) to change the colors
@@ -41,14 +46,40 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
 
   function self.UpdateComponents()
     self.UpdateDimensions()
+    self.draw_frame.SetSize(1, 0, 1, 0)
+    self.draw_frame.UpdateDimensions()
     self.components.top.SetSize(1, 0, 0, IndentBlock.TOP_BORDER)
     self.components.top.SetPosition(0, 0, 0, 0)
     self.components.left.SetSize(0, IndentBlock.LEFT_BORDER, 1, -IndentBlock.TOP_BORDER)
     self.components.left.SetPosition(0, 0, 0, IndentBlock.TOP_BORDER)
     self.components.bottom.SetSize(1, -IndentBlock.LEFT_BORDER, 0, IndentBlock.BOTTOM_BORDER)
     self.components.bottom.SetPosition(0, IndentBlock.LEFT_BORDER, 1, -IndentBlock.BOTTOM_BORDER)
-    self.components.right.SetSize(0, IndentBlock.LEFT_BORDER, 0, 50)
-    self.components.right.SetPosition(1, -IndentBlock.LEFT_BORDER, 1, -50)
+  end
+
+  function self.GetChildrenVerticalSpacing()
+    local spacing = 0
+    for i, v in ipairs(self.contained) do
+      if #v.contained == 0 then
+        spacing = spacing + v.abs_size.y
+      else
+        spacing = spacing + v.GetChildrenVerticalSpacing() + IndentBlock.BOTTOM_BORDER + IndentBlock.TOP_BORDER
+      end
+    end
+    return spacing
+  end
+
+  function self.ShiftContainedY()
+    local last = IndentBlock.TOP_BORDER
+    for i, v in ipairs(self.contained) do
+      v.position.y.offset = last
+      v.ShiftContainedY()
+      last = last + v.abs_size.y
+    end
+    self.size.y.offset = self.GetChildrenVerticalSpacing() + IndentBlock.TOP_BORDER + IndentBlock.BOTTOM_BORDER
+    if (self.size.y.offset <= IndentBlock.TOP_BORDER + IndentBlock.BOTTOM_BORDER) then
+      self.size.y.offset = IndentBlock.TOP_BORDER + IndentBlock.BOTTOM_BORDER + 50
+    end
+    self.UpdateComponents()
   end
 
   function self.AddChild(c)
@@ -56,16 +87,23 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
     c.position.x.scale = 0
     c.position.x.offset = IndentBlock.LEFT_BORDER
     c.position.y.scale = 0
-    c.position.y.offset = IndentBlock.TOP_BORDER
     c.size.x.scale = 1
     c.size.x.offset = -IndentBlock.LEFT_BORDER
-    c.size.y.scale = 1
-    c.size.y.offset = -(IndentBlock.BOTTOM_BORDER + IndentBlock.TOP_BORDER + 25)
-    c.UpdateDimensions()
+    c.size.y.scale = 0
+    c.size.y.offset = 0
     if c.InstanceOf("IndentBlock") then
       c.draw_frame.UpdateDimensions()
+      table.insert(self.contained, c)
+      self.ShiftContainedY()
+    else
+      table.insert(self.children, c)
     end
-    table.insert(self.children, c)
+  end
+
+  function self.UpdateContained()
+    for i, v in ipairs(self.contained) do
+      v.UpdateDimensions()
+    end
   end
 
   function self.GetTopLeftInner()
@@ -73,7 +111,8 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
   end
 
   function self.Draw()
-    for i, v in pairs(self.GetChildren()) do
+    self.draw_frame.Draw()
+    for i, v in pairs(self.contained) do
       v.Draw()
     end
   end
@@ -82,7 +121,6 @@ function IndentBlock.new(ssx, sox, ssy, soy, psx, pox, psy, poy)
   self.draw_frame.AddChild(self.components.top)
   self.draw_frame.AddChild(self.components.bottom)
   self.draw_frame.AddChild(self.components.left)
-  self.draw_frame.AddChild(self.components.right)
   self.draw_frame.SetVisible(false)
   self.draw_frame.DrawChildrenWhenNotVisible(true)
   self.UpdateComponents()
